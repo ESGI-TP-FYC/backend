081d4f093803956861b9ebd3f25c0cf47888a3df
HLimam <heithem.limame@gmail.com>
2023-02-12 20:29:48 +0100

    update V2

--- src/test/java/fr/esgi/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/fr/esgi/ecommerce/EcommerceApplicationTests.java
@@ -1,17 +1,14 @@
 package fr.esgi.ecommerce;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EcommerceApplicationTests {
 
-    @Test
+    @Disabled
     void contextLoads() {
     }
 
-    @Test
-    public void main() {
-        EcommerceApplication.main(new String[] {});
-    }
 }
